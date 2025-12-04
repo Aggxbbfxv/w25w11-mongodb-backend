a42288825a322c06c4cac8367cdef6af5f791dd5
Aggxbbfxv <miaow12255@gmail.com>
2025-12-04 16:47:07 +0900

    🚧CORS 추가

--- src/main/kotlin/kr/ac/kumoh/s20220300/w25w11_mongodb_backend/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20220300/w25w11_mongodb_backend/controller/SongController.kt
@@ -16,7 +16,10 @@ import java.net.URI
 
 @RestController
 @RequestMapping("/api/songs")
-@CrossOrigin(origins = ["http://localhost:5173"])
+@CrossOrigin(origins = [
+    "http://localhost:5173",
+    "https://Aggxbbfxv.github.io/w25w12-songlist"
+])
 class SongController(
     private val service: SongService
 ) {
